8e2d2d8688b136a67efa9a5fdab330d8000cdaac
Debaditya Ghosh <gdebaditya16@gmail.com>
2025-03-09 18:24:11 +0530

    First Commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# Xpert-Diet1
---
+++ script.js
@@ -0,0 +1,10 @@
+
+const show = document.querySelector('#submit');
+show.addEventListener("click", (event) => {
+    document.style.backgroundColor = "red";
+    console.log("clicked");
+});
+
+
+
+
